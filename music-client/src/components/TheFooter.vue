1b643408751aceeb60061f574da3d0f99df7e644
Vikincer <vikinc@163.com>
2022-07-10 21:54:50 +0800

    Test

---
+++ music-client/src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="the-footer">
+        <p>-vi_misic-</p>
+        <p>可联系：Vikinc@163.com</p>
+    </div>
+</template>
+<script>
+export default {
+  name: 'the-footer'
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/css/the-footer.scss';
+</style>
